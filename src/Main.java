843b82f73d5e2bdc0895eefe213caf00be7fef17
Kirill Gureev <kir.gureev2010@ya.ru>
2022-06-20 21:43:21 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 7_600;
+        int toEarnMiles = 20;
+        int mile = ticket / toEarnMiles;
+        System.out.println("За покупку вашего билеты мы начислим: " + mile + " миль");
+    }
+}
